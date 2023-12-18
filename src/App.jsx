06ed8e2e570f995d48682d407d566c5a35b26dbb
armine1002 <115899041+armine1002@users.noreply.github.com>
2023-12-18 15:57:32 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Armine Meseldzhyan",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "arminem.2001@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "armine meseldzhyan",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
